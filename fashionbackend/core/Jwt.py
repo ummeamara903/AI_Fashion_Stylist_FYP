5b36e4e8be92d5e317f5034f13a2dd4093111336
amara <uammara650@gmail.com>
2026-06-29 08:32:27 +0500

    Move Configuration

--- fashionbackend/core/Jwt.py
+++ fashionbackend/core/Jwt.py
@@ -1,13 +1,20 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi import HTTPException
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
 
 # =========================
 # CONFIG
 # =========================
-SECRET_KEY = "a8f9d2k3l4m5n6p7q8r9s0t1u2v3w4x5"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
 
 
 # =========================
--- fashionbackend/core/database.py
+++ fashionbackend/core/database.py
@@ -1,13 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:uammara650@localhost:5432/fashion_db"
-# DATABASE_URL=postgresql+psycopg2://postgres:uammara650@localhost:5432/ai_fashion_db
+# Load environment variables
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
 Base = declarative_base()
-
-
